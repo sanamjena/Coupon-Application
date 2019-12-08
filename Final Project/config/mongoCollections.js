c794d7a63bccefc2ccc795d4a590e2bec4f83da1
Sanam Sritam Jena <sanamsritam@gmail.com>
2019-12-08 15:16:12 -0500

    DataBase

--- Final Project/config/mongoCollections.js
+++ Final Project/config/mongoCollections.js
@@ -17,6 +17,6 @@ const getCollectionFn = collection => {
 
 /* Now, you can list your collections here: */
 module.exports = {
-  //posts: getCollectionFn("posts"),
-  animals: getCollectionFn("animals")
+  likes: getCollectionFn("likes"),
+  users: getCollectionFn("users")
 };
