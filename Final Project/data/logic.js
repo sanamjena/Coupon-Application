0e1ed2648deb3d7d997895fd3144ac6a31eaddef
Sanam Sritam Jena <sanamsritam@gmail.com>
2019-12-08 16:09:21 -0500

    Logic Update

--- Final Project/data/logic.js
+++ Final Project/data/logic.js
@@ -2,35 +2,59 @@ const mongoCollections = require("../config/mongoCollections");
 const MongoClient = require("mongodb").MongoClient;
 const users = mongoCollections.users;
 const likes = mongoCollections.likes;
+const { ObjectId } = require("mongodb");
+
+async function get(Id) {
+  if (!Id) throw "You must provide an id to search for";
+  const userCollection = await users();
+
+  var objId = 0;
+  if (typeof Id == "string") {
+    objId = ObjectId.createFromHexString(Id);
+  } else {
+    objId = Id;
+  }
+  const usr = await userCollection.findOne({ _id: objId });
+  if (usr === null) throw "No Animals found";
+  return usr;
+}
 
 async function create(firstname, lastname, emailId, gender, city, state) {
-    if (!firstname) throw "You must provide a name for your animal";
-    if (
-        Number.isInteger(firstname) ||
-        Number.isInteger(lastname) ||
-        Number.isInteger(gender) ||
-        Number.isInteger(city) ||
-        Number.isInteger(state)
-    ) {
-        throw "Please enter a valid input. Either the first name or the animal type is of type number";
-    }
-    if (typeof animalType == "object") {
-        if (animalType.length > 1) throw "You can provide only one breed.";
-        animalType = animalType[0];
-    }
+  if (!firstname) throw "You must provide a name for your animal";
+  if (
+    Number.isInteger(firstname) ||
+    Number.isInteger(lastname) ||
+    Number.isInteger(gender) ||
+    Number.isInteger(city) ||
+    Number.isInteger(state)
+  ) {
+    throw "Please enter a valid input. Either the firstname, lastname, gender, city or state is of type number";
+  }
+  if (
+    typeof firstname == "object" ||
+    typeof lastname == "object" ||
+    typeof emailId == "object" ||
+    typeof gender == "object" ||
+    typeof city == "object"
+  ) {
+    throw "Please enter a valid Input. Either the firstname, lastname, gender, city or state is of type Object";
+  }
 
-    const animalCollection = await animals();
+  const userCollection = await users();
 
-    let newAnimal = {
-        name: name,
-        animalType: animalType
-    };
+  let newUser = {
+    firstname: firstname,
+    lastname: lastname,
+    emailId: emailId,
+    gender: gender,
+    city: city
+  };
 
-    const insertInfo = await animalCollection.insertOne(newAnimal);
-    if (insertInfo.insertedCount === 0) throw "Could not add Animal";
+  const insertInfo = await userCollection.insertOne(newUser);
+  if (insertInfo.insertedCount === 0) throw "Could not add User";
 
-    const newId = insertInfo.insertedId.toString();
+  const newId = insertInfo.insertedId.toString();
 
-    const animal = await get(newId);
-    return animal;
+  const newusr = await get(newId);
+  return newusr;
 }
